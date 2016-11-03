b2135a9e01b7875063a9757693f4338a9ba2c1fd
pipinkurniawati <13513089@std.stei.itb.ac.id>
2016-11-04 02:02:49 +0700

    refactor main class

--- Elliptic Curve Cryptography/src/elgamal/Main.java
+++ Elliptic Curve Cryptography/src/elgamal/Main.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  *
@@ -16,40 +17,80 @@ import java.util.List;
  */
 public class Main {
     public static void main(String[] args) throws IOException{
-        
-        IOFile file = new IOFile("data/plaintext.txt");
-        List<String> blocks = new ArrayList<String>(file.getDataSegment());
-        
-        System.out.println("Plaintext");
-        System.out.println(file.getData());        
-        
         EllipticCurve curve = new EllipticCurve(new BigInteger("2"), new BigInteger("1"), new BigInteger("32416190071"));
+        
         BigInteger privateKey1 = new BigInteger("13456789");
         BigInteger privateKey2 = new BigInteger("23456789");
-        BigInteger randomKey = new BigInteger("99");
-        Point basis = new Point(new BigInteger("0"), curve.calculateY(new BigInteger("0"))); 
         
+        BigInteger randomKey = new BigInteger("99");
+        
+        Point basis = new Point(new BigInteger("0"), curve.calculateY(new BigInteger("0"))); 
+
         Point publicKey1 = basis.multiply(privateKey2, new BigInteger("32416190071"), new BigInteger("2"));
         Point publicKey2 = basis.multiply(privateKey1, new BigInteger("32416190071"), new BigInteger("2"));
         
-        ECCElGamal encryption = new ECCElGamal();
-        String result = new String();
-        for (int i=0; i<blocks.size(); i++) {
-            encryption = new ECCElGamal(blocks.get(i), "", privateKey1, randomKey, basis, curve, publicKey1);  
-            encryption.encrypt();
-            result += encryption.getCiphertext();
+        Scanner scanner = new Scanner(System.in);
+        displayMenu();
+        String choice = scanner.nextLine();
+        while (!choice.equals("3")) {
+            switch (choice) {
+                case "1":
+                    System.out.println("\n********** Encryption **********\n");
+                    System.out.print("The file you want to encrypt: ");
+                    String infile = scanner.nextLine(); 
+                    System.out.println();
+                    IOFile file = new IOFile(infile);
+                    List<String> blocks = new ArrayList<String>(file.getDataSegment());
+                    System.out.println("------- Plaintext -------");
+                    System.out.println(file.getData()); 
+                    ECCElGamal encryption = new ECCElGamal();
+                    String result = new String();
+                    for (int i=0; i<blocks.size(); i++) {
+                        encryption = new ECCElGamal(blocks.get(i), "", privateKey1, randomKey, basis, curve, publicKey1);  
+                        encryption.encrypt();
+                        result += encryption.getCiphertext();
+                    }
+                    System.out.println("\n------- CipherText -------");
+                    System.out.println(result);
+                    System.out.print("\nSave the result to : ");
+                    String outfile = scanner.nextLine(); 
+                    file = new IOFile(outfile);
+                    System.out.println("Saving the result...");
+                    file.writeFile(result);
+                    break;
+                case "2":
+                    System.out.println("\n********** Decryption **********\n");
+                    System.out.print("The file you want to decrypt: ");
+                    infile = scanner.nextLine(); 
+                    System.out.println();
+                    file = new IOFile(infile);
+                    ECCElGamal decryption = new ECCElGamal("", file.readFile(), privateKey2, randomKey, basis, curve, publicKey2);
+                    decryption.decrypt();
+                    System.out.println("\n------- Plaintext -------");
+                    System.out.println(decryption.bigIntToString());
+                    System.out.print("\nSave the result to : ");
+                    outfile = scanner.nextLine(); 
+                    file = new IOFile(outfile);
+                    System.out.println("Saving the result...");
+                    file.writeFile(decryption.bigIntToString());
+                    break;
+                default:
+                    System.out.println("Your input is not recognizable");
+                    break;
+                    
+            }
+            System.out.println();
+            displayMenu();
+            choice = scanner.nextLine(); 
+            System.out.println();
         }
-        file = new IOFile("result.txt");
-        file.writeFile(result);
-        
-        System.out.println("\n------- Enkripsi -------\n");
-        System.out.println("CipherText");
-        System.out.println(result);
-        
-        System.out.println("\n------- Dekripsi -------\n");
-        file = new IOFile("result.txt");
-        ECCElGamal decryption = new ECCElGamal("", file.readFile(), privateKey2, randomKey, basis, curve, publicKey2);
-        decryption.decrypt();
-        System.out.println(decryption.bigIntToString());
+    }
+    
+    public static void displayMenu() {
+        System.out.println("CHOOSE COMMAND");
+        System.out.println("1. Encrypt a Plaintext");
+        System.out.println("2. Decrypt a Ciphertext");
+        System.out.println("3. Exit");
+        System.out.print("your input: ");
     }
 }
